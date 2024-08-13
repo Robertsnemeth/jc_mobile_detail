d55c3d9604d8e12c6324fc58b00fbda3522db9c9
Robertsnemeth <104597057+Robertsnemeth@users.noreply.github.com>
2024-08-12 19:15:42 -0700

    changed naming of services, added level 3 interior and exterior, full +, cermaic. Added photos for cermaic

--- app/services/page.js
+++ app/services/page.js
@@ -19,6 +19,8 @@ import After2 from '../../public/IMG_4421.webp';
 import After3 from '../../public/IMG_3849.webp';
 import After4 from '../../public/IMG_4343.webp';
 import After5 from '../../public/IMG_4372.webp';
+import Cermaic1 from '../../public/IMG_8422.webp';
+import Cermaic2 from '../../public/IMG_8399.jpg';
 
 
 
@@ -28,79 +30,58 @@ export default function Services () {
     const Slider = dynamic(() => import('../components/slider'))
     const Service = dynamic(() => import('../components/service'))
 
-    const fullDetail = {
-        title: "Full Detail",
+    const interiorExteriorLevel1 = {
+        title: "Interior & Exterior Level 1 (Full)",
         description: "Our full detail service is to completely renew and revitalize all surfaces of your vehicle. This service focuses on cleaning, decontaminating, reconditioning and applying protection to the outside of your vehicle and deep cleaning the interior of your car.",
         vehicles: [{
             car: Car1,
-            price: 225
+            price: 245
         },
         {
             car: Car2,
-            price: 260
-        },
-        {
-            car: Car3,
             price: 300
         },
         {
+            car: Car3,
+            price: 325
+        },
+        {
             car: Car4,
-            price: 350
+            price: 375
         }]
     }
 
-    const basicInterior = {
-        title: "Basic Interior",
-        description: "This is a light detailing of the interior, we thoroughly vacuum the entire interior, wash floor mats, clean windows, and apply protectant to interior trim.",
+    const interiorExteriorLevel2 = {
+        title: "Interior & Exterior Level 2 (Full+)",
+        description: "Estimated Duration: 3 hours and 30 minutes. Exterior includes: Wheel and Tire Cleaning + Wheel Well Cleaning, Pre Wash (No Contact), Bug Removal, Foam Wash, Door Jamb Wipe Down, Exterior Window Cleaning, Clay Bar (Removes Embedded Contaminants), 3-6 Month Spray Sealant. Interior includes: Full Vacuum including Air Blowout, Interior Window Cleaning, All Hard Surfaces Scrubbed and Wiped. Seat Cleaning includes: Crevice Cleaning. ",
         vehicles: [{
             car: Car1,
-            price: 100
+            price: 370
         },
         {
             car: Car2,
+            price: 445
+        },
+        {
+            car: Car3,
+            price: 505
+        },
+        {
+            car: Car4,
+            price: 565
+        }]
+    }
+
+    const interiorLevel1 = {
+        title: "Interior Level 1",
+        description: "Interior includes: Full Vacuum including Air Blowout, Interior Window Cleaning, All Hard Surfaces Scrubbed and Wiped. Seat Cleaning includes: Crevice Cleaning.",
+        vehicles: [{
+            car: Car1,
             price: 125
         },
         {
-            car: Car3,
-            price: 170
-        },
-        {
-            car: Car4,
-            price: 200
-        }]
-    }
-
-    const deluxeInterior = {
-        title: "Deluxe Interior",
-        description: "We address every single surface of the interior. We start with a deep vacuuming of the whole interior, wash floor mats and spot clean staining, shampoo carpets and seats, clean and condition leather, scrub all plastics, clean dashboard and center console, clean door jambs, clean windows, apply protectant to interior trim, and do a final inspection to ensure quality.",
-        vehicles: [{
-            car: Car1,
-            price: 160
-        },
-        {
             car: Car2,
-            price: 185
-        },
-        {
-            car: Car3,
-            price: 220
-        },
-        {
-            car: Car4,
-            price: 250
-        }]
-    }
-
-    const basicExterior = {
-        title: "Basic Exterior",
-        description: "This is for those looking to greatly enhance the exterior appearance of their vehicle without going overboard. The result is a glossy, smooth surface, contaminant free, vibrant color, deep cleaned wheels and tires, and the application of a spray ceramic sealant with 6 months durability.",
-        vehicles: [{
-            car: Car1,
-            price: 120
-        },
-        {
-            car: Car2,
-            price: 145
+            price: 150
         },
         {
             car: Car3,
@@ -108,20 +89,41 @@ export default function Services () {
         },
         {
             car: Car4,
-            price: 225
+            price: 215
         }]
     }
 
-    const deluxeExterior = {
-        title: "Deluxe Exterior",
-        description: "This is our best value service for those who want to go above and beyond with their vehicle. This is a 1 stage polish resulting in a 60% paint correction (polish) that eliminates most scratches and defects plus the basic exterior detail. Finished off with a spray on ceramic infused wax that will leave your paint refreshed and protected. An additional stage polish (2 stage) can be added for an additional $75 and will include a buffed on ceramic infused wax.",
+    const interiorLevel2 = {
+        title: "Interior Level 2",
+        description: "Interior includes: Full Vacuum including Air Blowout, Light Carpet Shampoo, Light Upholstery Shampoo, Interior Window Cleaning, All Hard Surfaces Scrubbed and Wiped. Seat Cleaning includes: Crevice Cleaning, Door Jamb Wipe Down",
         vehicles: [{
             car: Car1,
-            price: 250
+            price: 175
         },
         {
             car: Car2,
-            price: 325
+            price: 200
+        },
+        {
+            car: Car3,
+            price: 235
+        },
+        {
+            car: Car4,
+            price: 265
+        }]
+    }
+
+    const interiorLevel3 = {
+        title: "Interior Level 3 (Odor Removal)",
+        description: "Interior includes: Full Vacuum including Air Blowout, Heavy Carpet Shampoo, Heavy Upholstery Shampoo, Interior Window Cleaning, All Hard Surfaces Scrubbed and Wiped. Seat Cleaning includes: Crevice Cleaning, Door Jamb Wipe Down, OZONE Treatment (Neutralize & Kills Odors/Viruses/Bacteria)",
+        vehicles: [{
+            car: Car1,
+            price: 350
+        },
+        {
+            car: Car2,
+            price: 375
         },
         {
             car: Car3,
@@ -129,7 +131,91 @@ export default function Services () {
         },
         {
             car: Car4,
+            price: 425
+        }]
+    }
+
+    const exteriorLevel1 = {
+        title: "Exterior Level 1",
+        description: "Wheel and Tire Cleaning + Wheel Well Cleaning, Pre Wash (No Contact), Bug Removal, Foam Wash, Door Jamb Wipe Down, Exterior Window Cleaning, Clay Bar (Removes Emebedded Contaminants), 3-6 Month Spray Sealant.",
+        vehicles: [{
+            car: Car1,
+            price: 135
+        },
+        {
+            car: Car2,
+            price: 175
+        },
+        {
+            car: Car3,
+            price: 210
+        },
+        {
+            car: Car4,
+            price: 255
+        }]
+    }
+
+    const exteriorLevel2 = {
+        title: "Exterior Level 2 - Our Most Popular Package",
+        description: "Paint Enhacnment (1 Step Correction). 1 Step Correction can fix roughly 50% of paint inperfections (Swirling/Scratching/Water Spots). This package includes a Full Exterior Detail + Iron Decon and AIO (All In One) polish and sealant leaving your paint silky smooth and brigning clarity and vibrancy back to your paint. This is a great level for anyone looking to bring their daily vehicles paint back to life and protect their investment!  ",
+        vehicles: [{
+            car: Car1,
+            price: 300
+        },
+        {
+            car: Car2,
+            price: 400
+        },
+        {
+            car: Car3,
+            price: 475
+        },
+        {
+            car: Car4,
+            price: 550
+        }]
+    }
+
+    const exteriorLevel3 = {
+        title: "Exterior Level 3",
+        description: "Paint Enhacnment (2 Step Correction). 2 Step Correction can fix roughly 80%-90% of paint imperfections (Swirling/Scratching/Water Spots). This package includes a full exterior detail + Iron Decon, Wool Pad Rotary Cut to eliminate those stubborn defects, and finishing off your paint with an AIO (All In One) polish and sealant leaving your paint silky smooth and defect free. This package is perfectly paired with our Ceramic Coating, making your vehicle look better than ever before!",
+        vehicles: [{
+            car: Car1,
             price: 450
+        },
+        {
+            car: Car2,
+            price: 550
+        },
+        {
+            car: Car3,
+            price: 625
+        },
+        {
+            car: Car4,
+            price: 700
+        }]
+    }
+
+    const ceramicCoating = {
+        title: "Ceramic Coating",
+        description: "Ceramic Coating is the ultmate exterior protection for your vehicle. Extremely durable, ultra-shiny, super hydrophobic, and lasting up to 5+ years. Ceramic coatng has a number of benefits, from protection against harsh contaminants, to saving time on cleaning. Make your vehicle look immaculate with our Ceramic Coating package and pair it with a paint correction, and you will be sure to turn heads! ",
+        vehicles: [{
+            car: Car1,
+            price: 800
+        },
+        {
+            car: Car2,
+            price: 900
+        },
+        {
+            car: Car3,
+            price: 1000
+        },
+        {
+            car: Car4,
+            price: 1100
         }]
     }
 
@@ -142,40 +228,51 @@ export default function Services () {
         </div>
         <BreakPoint/>
         <div className='hidden lg:flex lg:gap-1 lg:items-center lg:px-[50px] lg:h-[500px]'>
-            <Service serviceType={fullDetail}/>
+            <Service serviceType={interiorExteriorLevel1}/>
             <Slider before={Before4} after={After4}/>
         </div>
         <div className='lg:hidden'>
             <Slider before={Before4} after={After4}/>
-            <Service serviceType={fullDetail}/>
+            <Service serviceType={interiorExteriorLevel1}/>
         </div>
+        <BreakPoint/>
+        <Service className='lg:px-[50px] lg:h-[500px]' serviceType={interiorExteriorLevel2}/>
         <BreakPoint/>
         <div className='lg:flex lg:gap-1 lg:items-center lg:px-[50px] lg:h-[500px] w-full'>
             <Slider before={Before5} after={After5}/>
-            <Service serviceType={basicInterior}/>
+            <Service serviceType={interiorLevel1}/>
         </div>
         <BreakPoint/>
         <div className='hidden lg:flex lg:gap-1 lg:items-center lg:px-[50px] lg:h-[500px]'>
-            <Service serviceType={deluxeInterior}/>
+            <Service serviceType={interiorLevel2}/>
             <Slider before={Before2} after={After2}/>
         </div>
         <div className='lg:hidden'>
             <Slider before={Before2} after={After2}/>
-            <Service serviceType={deluxeInterior}/>
+            <Service serviceType={interiorLevel2}/>
         </div>
+        <BreakPoint/>
+        <Service className='lg:px-[50px] lg:h-[500px]' serviceType={interiorLevel3}/>
         <BreakPoint/>
         <div className='lg:flex lg:gap-1 lg:items-center lg:px-[50px] lg:h-[500px]'>
             <Slider before={Before1} after={After1}/>
-            <Service serviceType={basicExterior}/>
+            <Service serviceType={exteriorLevel1}/>
         </div>
         <BreakPoint/>
         <div className='hidden lg:flex lg:gap-1 lg:items-center lg:px-[50px] lg:h-[500px]'>
-            <Service serviceType={deluxeExterior}/>
+            <Service serviceType={exteriorLevel2}/>
             <Slider before={Before3} after={After3}/>
         </div>
         <div className='lg:hidden'>
             <Slider before={Before3} after={After3}/>
-            <Service serviceType={deluxeExterior}/>
+            <Service serviceType={exteriorLevel2}/>
+        </div>
+        <BreakPoint/>
+        <Service className='lg:px-[50px] lg:h-[500px]' serviceType={exteriorLevel3}/>
+        <BreakPoint/>
+        <div className='lg:flex lg:gap-1 lg:items-center lg:px-[50px] lg:h-[500px]'>
+            <Slider before={Cermaic1} after={Cermaic2}/>
+            <Service serviceType={ceramicCoating}/>
         </div>
         <Link href="/booking" className='m-[5px]'>
             <Button>Book Now</Button> 
